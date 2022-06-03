e1bcbb34874c187d650daa5495cd77759be1d480
UillianLopes <uilliansl@outlook.com>
2022-06-03 15:39:27 -0300

    initial commit

---
+++ apps/mtg-live-life-counter/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mtg-live-life-counter-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'mtg-live-life-counter';
+}
